1ee59015980845e987ff9e090d01ad8ebbca8b28
LL <123456@163.com>
2019-06-14 22:53:39 +0800

    init

---
+++ src-button/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button} from 'antd'
+
+export default class App extends React.Component{
+
+  render(){
+    return (
+      <div className="App">
+          <Button type="primary">Button</Button>
+      </div>
+    );
+  
+  }
+}
