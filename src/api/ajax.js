a9bca941894134c757222a0d5cda39435af61026
LL <123456@163.com>
2019-06-17 18:23:58 +0800

    init

---
+++ src/api/ajax.js
@@ -0,0 +1,49 @@
+// 通用的能发送任何ajax请求的函数模块
+//封装axios库
+import axios from 'axios'
+
+export default function ajax(url, data = {}, method = "GET") {
+    return new Promise((resolve, reject) => {
+        let promise
+        //1.执行异步ajax请求(使用axios)
+        //发get请求
+        if (method === 'GET') {
+            promise = axios.get(url, {
+                params: data //指定quey参数
+            })
+        } else {
+            //发post请求
+            promise = axios.post(url, data)
+        }
+        promise.then(
+            response => {
+                //2.如果成功了，调用resolve()，并指定成功的数据
+                resolve(response.data)
+            },
+            error => {
+                //3.如果出错了，不调用时reject()，显示错误的提示
+                alert('请求出错：' + error.message)
+            }
+        )
+    })
+}
+
+
+
+async function login() {
+    // try {
+    //     const response = await ajax('./login', {
+    //         username: 'admin',
+    //         password: 'admin'
+    //     }, 'POST')
+    // } catch (error) { //处理请求异常
+    //     alert('请求出错：' + error.message)
+    // }
+
+    const result = await ajax('/login', {username: 'admin',password: 'admin'}, 'POST')
+    if (result.status === 0) {
+
+    } else {
+
+    }
+}
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,32 +1,83 @@
 import React from 'react'
-import { Form, Icon,Input,Button} from 'antd'
+import { Form, Icon, Input, Button } from 'antd'
 import logo from "./images/logo.png";
 import './login.less'
 
 
-export default class Login extends React.Component {
-    render(){
+
+class Login extends React.Component {
+    handleSubmit = (event) => {
+        //阻止默认性行为
+        event.preventDefault()
+        /* //收集输入的数据
+        const Username = this.props.form.getFieldValue('Username')
+        const password = this.props.form.getFieldValue('password')
+        const values = this.props.form.getFieldsValue()
+        console.log(Username, password, values); */
+
+        //统一进行所有表单项的验证
+        this.props.form.validateFields((err, value) => {
+            if (!err) {
+                console.log('发送登录Ajax请求：', value)
+            }
+        })
+    }
+    validator = (rule, value, callback) => {
+        value = value.trim()
+        if (!value) {
+            callback('必须输入密码!')
+        } else if (value.length < 4) {
+            callback('密码长度不能小于4位!')
+        } else if (value.length > 12) {
+            callback('密码长度不能大于12位!')
+        } else if (!/^[a-zA-Z0-9]+$/.test(value)) {
+            callback('用户名必须是英文，数字或下划线组成!')
+        } else {
+            callback()
+        }
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-        <div className="login">
-            <header className="login-header">
-                <img src={logo} alt="logo"/>
-                <h1>React项目: 后台管理系统</h1>
-            </header>
-            <section className="login-content">
-                <h2>用户登陆</h2>
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <Form.Item>
-                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username"/>
-                    </Form.Item>
-                    <Form.Item>
-                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码"/>
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
-                    </Form.Item>
-                </Form>
-            </section>
-        </div>
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo" />
+                    <h1>React项目: 后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登陆</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('Username', {
+                                initialValue: '',
+                                rules: [
+                                    { required: true, whitespace: true, message: '请输入用户名!' },
+                                    { min: 4, message: '用户名不能小于4位!' },
+                                    { max: 12, message: '用户名不能大于12位!' },
+                                    { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文，数字或下划线组成!' }
+                                ]
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                initialValue: '',
+                                rules: [
+                                    { validator: this.validator }
+                                ]
+                            })(
+                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
         );
     }
 }
+const WrapperLogin = Form.create()(Login);
+export default WrapperLogin
